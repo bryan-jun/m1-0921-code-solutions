98fcb96bf38d5405dec0aceac3f62b30e789580a
Bryan Jun <bryjun@gmail.com>
2021-10-18 20:58:37 -0700

    completed js operators & expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,52 @@
+var width = 3;
+var height = 2;
+
+var area = width * height;
+
+console.log('value of area: ', area);
+console.log('typeof area: ', typeof area);
+
+var bill = 10;
+var payment = 20;
+
+var change = payment - bill;
+
+console.log('value of change: ', change);
+console.log('typeof change: ', typeof change);
+
+var quizzes = 90;
+var midterm = 95;
+var final = 97;
+
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade: ', grade);
+console.log('type of grade: ', typeof grade);
+
+var firstName = 'Bryan';
+var lastName = 'Jun';
+var fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName: ', fullName);
+console.log('typeof fullName: ', typeof fullName);
+
+var pH = 6;
+
+var isAcidic = pH < 7;
+
+console.log('value of isAcidic: ', isAcidic);
+console.log('typeof isAcidic: ', typeof isAcidic);
+
+var headCount = 6;
+
+var isSparta = headCount === 300;
+
+console.log('value of isSparta: ', isSparta);
+console.log('type of isSparta: ', typeof isSparta);
+
+var motto = fullName;
+
+motto += ' is the GOAT';
+
+console.log('value of motto: ', motto);
+console.log('typeof motto: ', typeof motto);
