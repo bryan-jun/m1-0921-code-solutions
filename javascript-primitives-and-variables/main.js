1f4c8e5aa3d717ea8c13e87a8d7966cf0aab0863
Bryan Jun <bryjun@gmail.com>
2021-10-18 18:58:42 -0700

    completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,23 @@
+var fullName = 'Bryan Jun';
+
+var isCool = true;
+
+var totalPets = 1;
+
+console.log('Full name:', fullName);
+console.log('Is cool:', isCool);
+console.log('Total pets:', totalPets);
+
+console.log('type of fullName:', typeof fullName);
+console.log('type of isCool:', typeof isCool);
+console.log('type of totalPets:', typeof totalPets);
+
+var unknown = null;
+
+console.log('null:', unknown);
+console.log('type of unknown:', typeof unknown);
+
+var never;
+
+console.log('never:', never);
+console.log('type of never:', typeof never);
