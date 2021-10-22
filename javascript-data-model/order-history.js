324522535c95bb67ae0c5081b789a0ede901c591
Bryan Jun <bryjun@gmail.com>
2021-10-22 12:00:53 -0400

    completed js data model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,47 @@
+var orderHistory = [
+  {
+    orders: [
+      {
+        name: 'Javascript for impatient programmers',
+        delivered: 'Aug 8, 2020',
+        price: 31.55
+      }
+    ]
+  },
+
+  {
+    orders: [
+      {
+        name: 'The Timeless Way of Building',
+        delivered: 'Jul 20, 2020',
+        price: 41.33
+      }
+    ]
+  },
+
+  {
+    orders: [
+      {
+        name: 'Gamecube Controller Adapter',
+        delivered: 'Jul 7, 2020',
+        price: 15.98
+      }
+    ]
+  },
+
+  {
+    orders: [
+      {
+        name: 'Gamecube Controller',
+        delivered: 'Jul 5, 2020',
+        price: 94.95
+      },
+      {
+        name: 'The Art of SQL',
+        delivered: 'Jul 5, 2020',
+        price: 33.99
+      }
+    ]
+  }
+
+];
